629fcee06a488cbafe0f7901e55d419a0fa181ee
Lucas Chacon <lucaschacon37@gmail.com>
2026-09-25 16:05:32 -0300

    Colocando autenticação

--- src/main/java/br/com/uniciv/rest/livraria/heroku/Main.java
+++ src/main/java/br/com/uniciv/rest/livraria/heroku/Main.java
@@ -1,6 +1,20 @@
 package br.com.uniciv.rest.livraria.heroku;
 
+import java.io.File;
+import java.util.Arrays;
+
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
@@ -10,14 +24,34 @@ import org.eclipse.jetty.webapp.WebAppContext;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        // The port that we should run on can be set into an environment variable
-        // Look for that variable and default to 8080 if it isn't there.
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
+    	
+    	File keystoreFile = new File("server.keystore");
+    	
+        final Server server = new Server();
+        
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setSecureScheme("https");
+        httpConfig.setSecurePort(8443);
+        
+        ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
+        http.setPort(8080);
+        
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(keystoreFile.getAbsolutePath());
+        //sslContextFactory.setKeyStoreType("PKCS12");
+        sslContextFactory.setKeyStorePassword("livraria");
+        sslContextFactory.setKeyManagerPassword("livraria");
+        
+        HttpConfiguration httpsConfig = new HttpConfiguration(httpConfig);
+        
+        ServerConnector https = new ServerConnector(server, 
+        		new SslConnectionFactory(sslContextFactory,
+        				HttpVersion.HTTP_1_1.asString()),
+        		new HttpConnectionFactory(httpsConfig));
+        https.setPort(8443);
+        
+        server.setConnectors(new ServerConnector[] {http, https});
 
-        final Server server = new Server(Integer.valueOf(webPort));
         final WebAppContext root = new WebAppContext();
 
         root.setContextPath("/livraria-virtual");
@@ -31,8 +65,29 @@ public class Main {
         final String webappDirLocation = "src/main/webapp/";
         root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
         root.setResourceBase(webappDirLocation);
+        
+        HashLoginService loginService = new HashLoginService("MyRealm");
+        loginService.setConfig("myrealm.properties");
+        
+        server.addBean(loginService);
+        
+        Constraint constraint = new Constraint();
+        constraint.setName("auth");
+        constraint.setAuthenticate(true);
+        constraint.setRoles(new String[] {"admin", "user"});
+        
+        ConstraintMapping mapping = new ConstraintMapping();
+        mapping.setPathSpec("/*");
+        mapping.setConstraint(constraint);
+        
+        ConstraintSecurityHandler security = new ConstraintSecurityHandler();
+        server.setHandler(security);
+        
+        security.setConstraintMappings(Arrays.asList(mapping));
+        security.setAuthenticator(new BasicAuthenticator());
+        security.setLoginService(loginService);
 
-        server.setHandler(root);
+        security.setHandler(root);
 
         server.start();
         server.join();
